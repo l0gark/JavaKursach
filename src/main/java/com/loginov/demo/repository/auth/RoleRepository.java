e262737986c240ded3d5e519a9ee1a9f54844806
l0gark <l0gark@ya.ru>
2020-05-17 23:40:45 +0300

    remove unused files

--- src/main/java/com/loginov/demo/repository/auth/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.loginov.demo.repository.auth;
-
-import com.loginov.demo.model.auth.Role;
-import com.loginov.demo.model.auth.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/loginov/demo/repository/auth/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.loginov.demo.repository.auth;
-
-import com.loginov.demo.model.auth.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
